f55fc91afa7bae64856280c6576bf06e64a0b57b
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-19 20:29:26 -0600

    Add code to register urls/:id handler

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => { // register a "urls/:id route" :id means that the value in this part of the url will be available in req.params object
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}; 
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

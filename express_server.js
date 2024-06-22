b6e97158470f15238b7f5be25b601750c83d0066
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-22 11:22:41 -0600

    Add endpoint to handle a POST to /login

--- express_server.js
+++ express_server.js
@@ -77,6 +77,11 @@ app.post("/urls/:id/edit", (req, res) => {
 
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls/");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

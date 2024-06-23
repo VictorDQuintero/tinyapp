4660fad1888b97ccbcfe48d89f3c955a30e2270e
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-23 02:40:20 -0600

    Modify GET /urls route to display username from cookie

--- express_server.js
+++ express_server.js
@@ -21,9 +21,7 @@ const cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
-
-// app.use(cookieParser());
-
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -43,16 +41,18 @@ app.get("/urls.json", (req, res) => { // register a handler on /urls.json path
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
+
 app.get("/urls/new", (req, res) =>{ // register a urls/new route and responds with rendering urls_new template
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => { // register a "urls/:id route" :id means that the value in this part of the url will be available in req.params object
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
 

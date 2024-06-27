80cc13df5de8c8b2b4e0679a68f4cb4a9691ca2a
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-06-27 17:49:34 -0600

    Create helper.js, Add findUserByEmail function to it

---
+++ helper.js
@@ -0,0 +1,11 @@
+const findUserByEmail = function(email, database) {
+  for (const userId in database) {
+    const user = database[userId];
+    if (user.email === email) { // user trying to register with an email already in database
+      return user;
+    }
+  }
+  return null;
+};
+
+module.exports = {findUserByEmail};
